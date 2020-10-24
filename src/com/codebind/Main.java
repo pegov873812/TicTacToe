de38db8adcb4a7676a88c09968c859ce9bd0e425
pegov873812@gmail.com <pegov873812@gmail.com>
2020-10-24 10:39:31 +0300

    Первоначальная загрузка проекта

---
+++ src/com/codebind/Main.java
@@ -0,0 +1,12 @@
+package com.codebind;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
