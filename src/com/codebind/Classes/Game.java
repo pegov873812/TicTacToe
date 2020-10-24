6072ce7916cc38d27162b3c7bcfef28c817a24b9
pegov873812@gmail.com <pegov873812@gmail.com>
2020-10-24 12:08:41 +0300

    Реализован конец игры

--- src/com/codebind/Classes/Game.java
+++ src/com/codebind/Classes/Game.java
@@ -1,9 +1,17 @@
 package com.codebind.Classes;
 
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class Game {
     private String playerSymbol;
-    public Game() {
+    private String[][] gameField;
+    int winningResult;
+    public Game(int size, int winningResult) {
         playerSymbol = "X";
+        gameField = new String[size][size];
+        this.winningResult = winningResult;
+
     }
     public void switchPlayerSymbol() {
         if(playerSymbol == "X") playerSymbol = "O";
@@ -11,5 +19,25 @@ public class Game {
     }
     public String getPlayerSymbol() {
         return  playerSymbol;
+    }
+    public void updateGameField(int x, int y){
+        gameField[x][y] = playerSymbol;
+    }
+    public  void CheckWinner() {
+        if(CheckWinnerHorizontal()) {
+            JOptionPane.showMessageDialog(null,"Победил  " + playerSymbol);
+        }
+    }
+    private boolean CheckWinnerHorizontal(){
+        int counter = 0;
+        for(int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField[0].length; j++) {
+                if(gameField[i][j] == playerSymbol) counter++;
+                else  counter = 0;
+                if(counter == winningResult) return true;
+            }
+            counter = 0;
+        }
+        return  false;
     }
 }
--- src/com/codebind/UI/GamePenel.java
+++ src/com/codebind/UI/GamePenel.java
@@ -13,16 +13,29 @@ import java.util.ArrayList;
 public class GamePenel extends JPanel {
     public Game game;
     public GamePenel(int size) {
-        game = new Game();
+        game = new Game(size,3 );
         setLayout(new java.awt.GridLayout(size, size));
+        int xCounter = 0,yCounter = 0;
         for (int i = 1; i <= size * size; ++i) {
             JButton b = new JButton();
             b.setFont(new Font("Arial", Font.PLAIN, 50));
             b.setPreferredSize(new Dimension(100,100));
+            b.setName(yCounter + "/" + xCounter);
+            xCounter++;
+            if(xCounter == size) {
+                xCounter = 0;
+                yCounter++;
+            }
             b.addActionListener(new java.awt.event.ActionListener() {
                 public void actionPerformed(java.awt.event.ActionEvent e) {
                     b.setText(game.getPlayerSymbol());
+                    String name = b.getName();
+                    int x = Integer.parseInt(name.split("/")[0]);
+                    int y = Integer.parseInt(name.split("/")[1]);
+                    game.updateGameField(x,y);
+                    game.CheckWinner();
                     game.switchPlayerSymbol();
+
                 }
             });
             add(b);
