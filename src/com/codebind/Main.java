6d6ab153abb7d26dda3cd83cd46779f07343ddcb
pegov873812@gmail.com <pegov873812@gmail.com>
2020-10-24 10:44:21 +0300

    Создана главная панель в которой будут отображаться все элементы интерфейса

--- src/com/codebind/Main.java
+++ src/com/codebind/Main.java
@@ -1,5 +1,7 @@
 package com.codebind;
 
+import com.codebind.UI.MainPanel;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
@@ -7,6 +9,18 @@ import java.awt.event.ComponentEvent;
 import java.util.Locale;
 
 public class Main {
+    /** Поле вывода главной нанели содержащей все элементы интерфейса */
+    public static MainPanel mainPanel;
+    /**
+     * Функция создающая интерфейс приложения
+     */
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(1000, 800));
+        mainPanel = new MainPanel();
+        frame.add(mainPanel);
+        frame.setVisible(true);
     }
 }
---
+++ src/com/codebind/UI/MainPanel.java
@@ -0,0 +1,13 @@
+package com.codebind.UI;
+
+import javax.swing.*;
+
+public class MainPanel extends JPanel {
+
+    /**
+     * Конструктор - создание нового объекта
+     */
+    public MainPanel() {
+
+    }
+}
